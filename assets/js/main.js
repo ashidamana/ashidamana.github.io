7188d7e8215f65f4fb559186f656d0d7c8096dd0
Flyer <flyer7@foxmail.com>
2017-06-02 09:33:43 +0800

    yungengie

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,3 +141,12 @@ $(function() {
 //  Tie.loader("aHR0cHM6Ly9hcGkuZ2VudGllLjE2My5jb20vcGMvbGl2ZXNjcmlwdC5odG1s", true);
 
 });
+
+var cloudTieConfig = {
+  url: document.location.href, 
+  sourceId: "",
+  productKey: "86a52c1e48c0450385a25e49e7c984cd",
+  target: "cloud-tie-wrapper"
+};
+var yunManualLoad = true;
+Tie.loader("aHR0cHM6Ly9hcGkuZ2VudGllLjE2My5jb20vcGMvbGl2ZXNjcmlwdC5odG1s", true);
