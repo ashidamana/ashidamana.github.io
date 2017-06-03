623f95d94dfc682b0d210983a911d56ab9865d67
Flyer <flyer7@foxmail.com>
2017-06-03 14:37:08 +0800

    gentie

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
-    var cloudTieConfig = {
-        url: document.location.href,
-        sourceId: "",
-        productKey: "86a52c1e48c0450385a25e49e7c984cd",
-        target: "cloud-tie-wrapper"
-    };
-    var yunManualLoad = true;
-    console.log(666);
-    Tie.loader("aHR0cHM6Ly9hcGkuZ2VudGllLjE2My5jb20vcGMvbGl2ZXNjcmlwdC5odG1s", true);
+    alert(1)
 });
