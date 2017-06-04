61d36c5494114cfaa41222536246b33471734922
Flyer <flyer7@foxmail.com>
2017-06-04 13:20:08 +0800

    avatar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
     //    随机头像
-    var base_img_url="https://blog.mana.love/img/avatar_";
-    var avatar=$('.avatar_a');
-    var str='<img src="'+base_img_url+parseInt(19*Math.random())+'.png'+'" alt="" class="avatar">';
-    avatar.empty().append(str);
+    var base_img_url = "https://blog.mana.love/img/avatar_";
+    var avatar = $('.avatar_a');
+    var str = '<img src="' + base_img_url + parseInt(19 * Math.random()) + '.png' + '" alt="" class="avatar">';
+    if ($(".avatar_a>img").length == 0) {
+        avatar.empty().append(str);
+    } else {
+        $(".avatar_a>img").attr('src', base_img_url + parseInt(19 * Math.random()) + '.png');
+    }
     console.log(str);
 });
