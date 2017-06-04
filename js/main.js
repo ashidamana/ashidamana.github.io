a3fb1884f19e2ec1773509a1a22f25caf448eb59
Flyer <flyer7@foxmail.com>
2017-06-04 15:41:24 +0800

    avatar

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     if ($(".avatar_a>img").length == 0) {
         avatar.empty().append(str);
     } else {
-        console.log(223333);
+        console.log(666);
         $(".avatar_a>img").attr('src', base_img_url + parseInt(19 * Math.random()) + '.png');
     }
     console.log(str);
