a4bfc608e58de7a1d77c864789f8b0d6d16fd700
Flyer <flyer7@foxmail.com>
2017-06-04 13:01:18 +0800

    avatar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+    //    随机头像
     var base_img_url="https://blog.mana.love/img/avatar_";
-    var avatar=$('.avatar');
-//    随机头像
-    avatar.attr('src',base_img_url+parseInt(19*Math.random())+'.png');
-//    console.log(avatar);
+    var avatar=$('.avatar_a');
+    var str='<img src="'+base_img_url+parseInt(19*Math.random())+'.png'+'" alt="" class="avatar">';
+    avatar.empty().append(str);
+    console.log(str);
 });
